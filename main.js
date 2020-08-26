21aeee08819161a12230f673bd2e3223597c8d97
Wanda <wmmcinturff@gmail.com>
2020-08-25 21:32:55 -0500

    created dino array

---
+++ main.js
@@ -0,0 +1,58 @@
+const dinoArray = [
+    {
+        image: "./dino-image/Albertosaurus.jpg",
+        name: "Albie",
+        type: "Albertosaurus",
+        age: 76,
+        owner: "Wanda",
+    },
+
+    {
+        image: "./dino-image/Ankylosaurus.jpg",
+        name: "Angie",
+        type: "Ankylosaurus",
+        age: 92,
+        owner: "Wanda",
+    },
+
+    {
+        image: "./dino-image/Archaeopteryx.jpg",
+        name: "Archie",
+        type: "Archaeopteryx",
+        age: 67,
+        owner: "Wanda",
+    },
+
+    {
+        image: "./dino-image/brachiosaurus.jpg",
+        name: "Brock",
+        type: "Brachiosaurus",
+        age: 123,
+        owner: "Wanda",
+    },
+
+    {
+        image: "./dino-image/Edmontosaurus.jpg",
+        name: "Edmund",
+        type: "Edmontosaurus",
+        age: 98,
+        owner: "Wanda",
+
+    },
+
+    {
+        image: "./dino-image/Iguanodon.jpg",
+        name: "Iggy",
+        type: "Iguanodon",
+        age: 57,
+        owner: "Wanda",
+    },
+
+    {
+        image: "./dino-image/Pteranadon.jpg",
+        name: "Pteri",
+        type: "Pteranadon",
+        age: 74,
+        owner: "Wanda",
+    },
+];
